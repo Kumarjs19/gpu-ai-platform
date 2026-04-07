8f5303c8ec879e48987aad059f55c34e32ec1c7d
Kumarjs19 <kumarjs@rocketmail.com>
2026-04-07 14:41:31 +0530

    Add PostgreSQL DB config

---
+++ api/db.py
@@ -0,0 +1,9 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:pass@db:5432/app")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
